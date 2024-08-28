f8d3d8a0d2e2cb9d0c12d674afb8188c530ee532
Amol Dike <amoldike002@gmail.com>
2024-08-28 16:20:31 +0530

    first-commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach((panel) => {
+    panel.addEventListener('click', () => {
+        removeActiceClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiceClasses() {
+    panels.forEach((panel) => {
+        panel.classList.remove('active')
+
+    })
+
+}
